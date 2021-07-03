187cf613632ffb50614a13a620d46dd1006e8e01
Thiago Salles <thiago.salles@gmail.com>
2021-07-03 17:25:53 -0300

    Adding missing rm statement for layered predictions

--- run.sh
+++ run.sh
@@ -11,6 +11,7 @@ libsvm_predict="/Users/tsalles/Programming/autoencoder-reducer/libsvm/svm-predic
 
 rm $dataset/preds_bl.out;
 rm $dataset/preds_ae.out;
+rm $dataset/preds_ae_layered.out;
 rm $dataset/preds_liblinear_bl.out
 rm $dataset/preds_liblinear_ae.out
 rm $dataset/preds_liblinear_ae_layered.out
@@ -20,9 +21,9 @@ rm $dataset/preds_libsvm_ae_layered.out
 
 for i in `seq 0 $((num_folds-1))`; do
   echo "Round $i"
-  python3 model.py -t $dataset/${num_folds}fold/train$i -T $dataset/${num_folds}fold/test$i -o $dataset/preds_bl.out -c 256 -S 64 -e 50 -v 0.3 -r $i -p $dataset/hist_bl$i;
-  python3 model.py -t $dataset/${num_folds}fold/train$i -T $dataset/${num_folds}fold/test$i -a 512 256 -b 128  -c 256 --autoencoder --pretrain-ae -E 50 -e 50 -v 0.3 -r $i -p $dataset/hist$i -B $dataset/reduced$i -o $dataset/preds_ae.out -s $dataset/ae_model$i -S 64;
-  python3 model_layered.py -t $dataset/${num_folds}fold/train$i -T $dataset/${num_folds}fold/test$i -a 512 256 -a 128 64 -a 32 16 -b 64 32 16 -c 256  --autoencoder --pretrain-ae -E 50 -e 50 -v 0.3 -r $i -p $dataset/hist_layered$i -B $dataset/reduced_layered$i -o $dataset/preds_ae_layered.out -s $dataset/ae_model_layered$i -S 64 -B $dataset/reduced_layered$i;
+  python3 model.py -t $dataset/${num_folds}fold/train$i -T $dataset/${num_folds}fold/test$i -o $dataset/preds_bl.out -c 512 -S 64 -e 50 -v 0.3 -r $i -p $dataset/hist_bl$i;
+  python3 model.py -t $dataset/${num_folds}fold/train$i -T $dataset/${num_folds}fold/test$i -a 1024 512 -b 256  -c 512 --autoencoder --pretrain-ae -E 50 -e 50 -v 0.3 -r $i -p $dataset/hist$i -B $dataset/reduced$i -o $dataset/preds_ae.out -s $dataset/ae_model$i -S 64;
+  python3 model_layered.py -t $dataset/${num_folds}fold/train$i -T $dataset/${num_folds}fold/test$i -a 1024 512 -a 256 128 -a 64 32 -b 128 64 32 -c 512  --autoencoder --pretrain-ae -E 50 -e 50 -v 0.3 -r $i -p $dataset/hist_layered$i -B $dataset/reduced_layered$i -o $dataset/preds_ae_layered.out -s $dataset/ae_model_layered$i -S 64 -B $dataset/reduced_layered$i;
 
 
   $liblinear_train $dataset/${num_folds}fold/train$i $dataset/svm-model$i 
